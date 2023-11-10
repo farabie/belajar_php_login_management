38cfa3e5a13f0efd755d8749cf3d72df543364f5
Muhammad Farabie <farabie024@gmail.com>
2023-11-11 01:03:13 +0700

    Registration Repository

---
+++ app/Domain/User.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace BieProject\Belajar\PHP\LoginManage\Domain;
+
+
+class User {
+    public string $id;
+    public string $name;
+    public string $password;
+    public string $email;
+    public string $address;
+
+}
+
+
+?>
